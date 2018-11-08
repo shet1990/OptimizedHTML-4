f5dc5b97135871c170eed5ca7b8877e48a28087b
shet1990 <shet1990@gmail.com>
2018-11-08 13:00:33 +0300

    header and footer

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,10 @@
 $(function() {
 
-	// Custom JS
-
+	$('.menu-1 a').hover(function(){
+		$('.menu-1 a').not(this).css('opacity','.5')
+	},
+		function(){
+			$('.menu-1 a').css('opacity','1')
+	});
+	
 });
